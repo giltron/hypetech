c45379db9fc5ae522328a3a1399c62d89a6423fc
Hassan J. Ndow <hjn5906@rit.edu>
2013-12-14 23:04:57 -0500

    Add project source code

---
+++ src/game/entities/Player.java
@@ -0,0 +1,86 @@
+package game.entities;
+
+import game.InputHandler;
+import gfx.Colors;
+import gfx.Screen;
+import level.Level;
+
+public class Player extends Mob 
+{
+	private InputHandler input;
+	private int color = Colors.get(-1,111,145,321);
+
+	public Player(Level level, int x, int y, InputHandler input)
+	{
+		super(level, "Player", x, y, 1);
+		this.input = input;
+
+
+	}
+
+
+	public void tick() 
+	{
+		int xa = 0;
+		int ya = 0;
+		
+		if(input.up.isPressed())
+		{
+			ya--;
+		}
+		
+		if(input.down.isPressed())
+		{
+			ya++;
+		}
+		
+		if(input.left.isPressed())
+		{
+			xa--;
+		}
+		
+		if(input.right.isPressed())
+		{
+			xa++;
+		}
+		
+		if(xa != 0 || ya != 0)
+		{
+			move(xa,ya);
+			isMoving = true;
+		}
+		
+		else
+		{
+			isMoving = false;
+		}
+		
+		
+	}
+
+	
+	public void render(Screen screen) 
+	{
+		int xTile = 0;
+		int yTile = 28;
+		int modifier = 8 * scale;
+		int xOffset = x - modifier / 2;
+		int yOffset = y - modifier / 2 - 4;
+		
+		//lower body
+		screen.render(xOffset, yOffset, xTile + yTile *32, color);
+		screen.render(xOffset + modifier, yOffset, (xTile + 1) + yTile *32, color);
+		
+		//upper body
+		screen.render(xOffset, yOffset + modifier, xTile + (yTile +1) *32, color);
+		screen.render(xOffset + modifier, yOffset + modifier, (xTile +1) + (yTile + 1) *32, color);
+		
+	}
+	
+	public boolean hasCollided(int xa, int ya) 
+	{
+		
+		return false;
+	}
+
+}
---
+++ src/level/tiles/BasicTile.java
@@ -0,0 +1,26 @@
+package level.tiles;
+
+import gfx.Screen;
+import level.Level;
+
+public class BasicTile extends Tile
+{
+	protected int tileId;
+	protected int tileColor;
+
+	public BasicTile(int id,int x,int y, int tileColor) 
+	{
+		super(id,false,false);
+		this.tileId = x+y;
+		this.tileColor = tileColor;
+		
+	}
+
+	
+	public void render(Screen screen, Level level, int x, int y) 
+	{
+		screen.render(x,y,tileId,tileColor);
+		
+	}
+
+}
